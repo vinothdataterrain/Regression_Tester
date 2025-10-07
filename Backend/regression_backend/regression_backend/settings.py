f8a847d622152a84c4f3bbded4df223fda8c833d
sathiyasri-04 <sathiyasri@dataterrain.com>
2025-10-07 13:01:35 +0530

    fixed token expire issue

--- Backend/regression_backend/regression_backend/settings.py
+++ Backend/regression_backend/regression_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,6 +150,10 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
