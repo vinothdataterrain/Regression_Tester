6be7325720a4e8d04f1da65e7079df7a6164b627
JagadeeshK-DT <jagadeesh@dataterrain.com>
2025-10-07 15:01:53 +0530

    Responsive added

--- Frontend/regression-frontend/src/pages/TestPage.jsx
+++ Frontend/regression-frontend/src/pages/TestPage.jsx
@@ -430,7 +430,7 @@ export default function TestPage() {
                     startIcon={<ArrowBackIcon />}
                     onClick={() => navigate('/projects')}
                     size="small"
-                    className="!rounded-full !border !p-0 md:!p-1"
+                    className="!rounded-full !border !p-0 md:!px-2"
                     >
                    { isMdscreen && "Back"}
                   </Button>
@@ -463,6 +463,7 @@ export default function TestPage() {
                     label={`${currentProject?.testcases?.length || 0} test cases`}
                     size="small"
                     color="primary"
+                    className="!bg-blue-200 !text-blue-500"
                   />
                   <Chip
                     label={`Created: ${new Date(currentProject?.created_at).toLocaleDateString()}`}
@@ -472,14 +473,15 @@ export default function TestPage() {
                 </Box>
               </Box>
               
-              <Box sx={{ display: 'flex', gap: 2 }}>
+              <Box sx={{ display: 'flex', gap: 2 }} className=" w-full md:w-auto justify-between">
                 <Button
                   variant="contained"
                   startIcon={<AddIcon />}
                   onClick={() => handleAddTestCase(currentProject)}
                   size="small"
+                  className="!min-w-[120px] !bg-blue-700"
                 >
-                  Add Test Case
+                  {isMdscreen ? "Add Test Case" : "Add"}
                 </Button>
                 <Tooltip title="Upload JSON file">
                   <Button
@@ -490,8 +492,9 @@ export default function TestPage() {
                       setSelectedProject(currentProject);
                     }}
                     size="small"
+                    className="!min-w-[120px]"
                   >
-                    Upload JSON
+                    {isMdscreen ?  "Upload JSON" : "Upload"}
                   </Button>
                 </Tooltip>
               </Box>
@@ -557,7 +560,7 @@ export default function TestPage() {
                     }}
                   >
                     <Box
-                    className="flex flex-col md:flex-row  justify-start md:justify-between"
+                    className="flex flex-col md:flex-row items-start justify-start md:justify-between"
                       sx={{
                        
                         alignItems: "center",
@@ -567,6 +570,7 @@ export default function TestPage() {
                     >
                       <Typography 
                         variant="h6" 
+                        className=" w-full md:w-auto"
                         sx={{ 
                           flexGrow: 1,
                           overflow: 'hidden',
@@ -579,7 +583,7 @@ export default function TestPage() {
                         {testCase?.name}
                       </Typography>
                       
-                      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, mr: 2 }}>
+                      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, mr: 2 }} className=" w-full md:w-auto px-2">
                         {reportPath && !uploadedFile && (
                           <Tooltip title="Download HTML Report">
                             <IconButton
